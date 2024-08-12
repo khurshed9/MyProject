68deab708c4be812eff4da5f56c1eb0e802f77ca
khurshed9 <khurshedgalchaev@gmail.com>
2024-08-12 15:40:07 +0500

    first commit

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿    var person = new Person();
+
+        person.FirstName = "Nabi";
+        person.LastName = "Murodov";
+        person.Age = 17;
+
+        person.GetInfo();
+
+
+
+
+    class Person
+    {
+
+        public string FirstName;
+
+        public string LastName;
+
+        public int Age;
+
+        public void GetInfo()
+        {
+            Console.WriteLine($"FirstName: {FirstName}, LastNAme : {LastName}, Age: {Age}");
+        }
+    }
+
+
+
