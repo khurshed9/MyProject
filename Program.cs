664f83ca0755db7ccae9b1f638fdab12a8f8bfc9
khurshed9 <khurshedgalchaev@gmail.com>
2024-08-12 15:43:44 +0500

    names

--- Program.cs
+++ Program.cs
@@ -1,22 +1,31 @@
-﻿    var person = new Person();
+﻿var person = new Person();
 
-        person.FirstName = "Nabi";
-        person.LastName = "Murodov";
-        person.Age = 17;
+person.FirstName = "Nabi";
+person.LastName = "Murodov";
+person.Age = 17;
+person.SetName("OOP");
+person.GetName();
+person.GetInfo();
 
-        person.GetInfo();
 
+class Person
+{
 
+    public string FirstName;
 
+    public string LastName;
 
-    class Person
+    public int Age;
+
+    public void SetName(string name)
     {
+        FirstName = name;
+    }
 
-        public string FirstName;
-
-        public string LastName;
-
-        public int Age;
+    public string GetName()
+    {
+        return FirstName;
+    }
 
         public void GetInfo()
         {
